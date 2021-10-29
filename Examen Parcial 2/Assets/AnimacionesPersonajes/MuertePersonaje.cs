c6f7bc96e84f8cb4922920c6a7d89b12e466fbfe
Cat3FD1365 <82287113+Cat3FD1365@users.noreply.github.com>
2021-10-29 11:56:54 -0500

    Espadas y Script
    Los personajes ya tienen espadas y agregué otro script que es para la muerte del enemigo.

--- Examen Parcial 2/Assets/AnimacionesPersonajes/MuertePersonaje.cs
+++ Examen Parcial 2/Assets/AnimacionesPersonajes/MuertePersonaje.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class MuertePersonaje : MonoBehaviour
 {
-    Animator anim;
+    Animator anim; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +22,4 @@ public class MuertePersonaje : MonoBehaviour
         
     }
 
-   
 }
