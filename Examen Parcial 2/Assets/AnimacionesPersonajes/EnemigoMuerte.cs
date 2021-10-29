5eaefc818fc43289650d2740002f1a62360ad868
Cat3FD1365 <82287113+Cat3FD1365@users.noreply.github.com>
2021-10-29 13:55:03 -0500

    Ya Muere
    El script quedó corregido y el enemigo ya muere cuando el personaje lo ataca. También cambie algunos colliders de los personajes que estaba usando.

--- Examen Parcial 2/Assets/AnimacionesPersonajes/EnemigoMuerte.cs
+++ Examen Parcial 2/Assets/AnimacionesPersonajes/EnemigoMuerte.cs
@@ -8,20 +8,35 @@ public class EnemigoMuerte : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        E_animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        /*if (Input.GetKey("w"))
+        {
+            E_animator.SetBool("IsDeath", true);
+        }*/
+
     }
 
-    void OnCollisionEnter(Collision collision)
+    /*void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Sword")
         {
-            E_animator.SetBool("IsDeath", true);
+            //E_animator.SetBool("IsDeath", true);
+            E_animator.SetTrigger("IsDeath2");
         }
+     
+    }*/
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Sword")
+        {
+            //E_animator.SetBool("IsDeath", true);
+            E_animator.SetTrigger("IsDeath2");
+        }
+
     }
 }
