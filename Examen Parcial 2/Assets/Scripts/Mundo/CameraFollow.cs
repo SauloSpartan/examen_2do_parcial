b240ff943065eff50978d155930bb499b16c5837
SauloTrujillo <SauloSpartan2@gmail.com>
2021-11-04 20:08:10 -0600

    Arreglando el Examen - Parte 1
    Arreglando sus errores de organizacón, codigo, animación, nombrado, etc

---
+++ Examen Parcial 2/Assets/Scripts/Mundo/CameraFollow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+    //Variable for offset
+    public Vector3 offset;
+    void Update()
+    {
+
+        gameObject.transform.position = GameObject.Find("Player").transform.position + offset;
+
+        Vector3 cameraPos = GameObject.Find("Main Camera").transform.position;
+
+        //This is the distance between player and camera in X, Y and Z
+        Vector3 playerPos = new Vector3(GameObject.Find("Main Camera").transform.position.x, 
+            gameObject.transform.position.y, gameObject.transform.position.z);
+        gameObject.transform.position = playerPos;
+
+        //To change camera distance just write trasnform.position.? +/- c
+    }
+}
